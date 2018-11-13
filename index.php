1bce1d3e898d27f89c047f972e9b79618d66271d
Unknown <unknown@example.com>
2018-11-14 01:03:44 +0900

    commit4

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
    $hensuA = 10;
-   $hensuB = $hensuA; •Ï”A ‚ğ•Ï”B ‚É‘ã“ü
-   $hensuA = 20; •Ï”A ‚Ì’l‚ª•Ï‰»‚·‚é‚ªA•Ï”B ‚É‰e‹¿‚Í–³‚¢
+   $hensuB = $hensuA;
+   $hensuA = 20;
    print "$hensuA <br>";
    print "$hensuB <br>";
 ?>
