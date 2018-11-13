a6deaf942148c0b868f40921b0a15f6112db180e
Unknown <unknown@example.com>
2018-11-14 00:58:30 +0900

    test2

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $hensuA = 10;
-   $hensuB = $hensuA; •Ï”A ‚ğ•Ï”B ‚É‘ã“ü
-   $hensuA = 20; •Ï”A ‚Ì’l‚ª•Ï‰»‚·‚é‚ªA•Ï”B ‚É‰e‹¿‚Í–³‚¢
-   print "$hensuA <br>";
-   print "$hensuB <br>";
-?>
