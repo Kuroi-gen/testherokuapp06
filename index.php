eeac74f980bbcc7e2c1e9f6e7879c31107522127
Unknown <unknown@example.com>
2018-11-14 00:55:59 +0900

    test
    test

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+   $hensuA = 10;
+   $hensuB = $hensuA; •Ï”A ‚ğ•Ï”B ‚É‘ã“ü
+   $hensuA = 20; •Ï”A ‚Ì’l‚ª•Ï‰»‚·‚é‚ªA•Ï”B ‚É‰e‹¿‚Í–³‚¢
+   print "$hensuA <br>";
+   print "$hensuB <br>";
+?>
